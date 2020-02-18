6db17e482a0c90d9c3fab727427616f6fa22c690
Senuri Wijenayake <senuri.wijenayake@gmail.com>
2020-02-18 22:25:38 +1100

    questions with screenshots

--- code.js
+++ code.js
@@ -87,7 +87,8 @@ exports.getBigFiveQuestions = function() {
 
 //Function to save user data
 exports.saveUserData = function(user) {
-  var q = [-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
+  // var q = [-1,0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26];
+  var q = [-1,0,1,2,3,4,5,6,7,8,9,10];
   var qOrder = shuffle(q);
 
   user.qOrder = qOrder;
